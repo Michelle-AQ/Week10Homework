4877b67fb5349bab0635275de2f65966bf771a5a
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-26 13:39:23 +0100

    add image of daughter

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ def hello(name):
     </html>
     """.format(name, url)
 
+    
 @app.route('/about_us')
 def about_us():
+    daughter_image_url = url_for('static', filename='daughter.jpg')
     return """
     <html>
     <head>
@@ -30,10 +32,10 @@ def about_us():
     <body>
         <h1>Michelle & Ellen</h1>
         <p>add text</p>
-        
+        <img src="{}" height='300' width='250' alt="my daughter">
     </body>
     </html>
-    """.format()
+    """.format(daughter_image_url)
 
 if __name__=='__main__':
     app.run(debug=True)
