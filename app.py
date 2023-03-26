930aba29663ebc538e130e08f626f79e1cdba3b8
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-26 10:50:32 +0100

    add about us page

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from Flask import Flask, Response, request
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello(name):
+    url = url_for('about_us')
     return """
     <html>
     <head>
@@ -12,12 +13,27 @@ def hello(name):
     <body>
         <h1>Name pages</h1>
         <p>Hello {}</p>
-        
-        <img src="daughter.jpg">
+        <a href= "{}">About Us</a>
+        <img src="./daughter.jpg">
         
     </body>
     </html>
-    """.format(name)
+    """.format(name, url)
+
+@app.route('/about_us')
+def about_us():
+    return """
+    <html>
+    <head>
+        <title>About Us</title>
+    </head>
+    <body>
+        <h1>Michelle & Ellen</h1>
+        <p>add text</p>
+        
+    </body>
+    </html>
+    """.format()
 
 if __name__=='__main__':
     app.run(debug=True)
