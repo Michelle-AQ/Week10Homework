bd55ee81b24227aec95f20e1d36a5a211bbaccfa
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-26 10:19:55 +0100

    return an html page

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "Hello"
+@app.route('/hello/<name>')
+def hello(name):
+    return """
+    <html>
+    <head>
+        <title>Sample - Flask routes</title>
+    </head>
+    <body>
+        <h1>Name pages</h1>
+        <p>Hello {}</p>
+    </body>
+    </html>
+    """.format(name)
 
 if __name__=='__main__':
     app.run(debug=True)
